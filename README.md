b3159b68973d0de034a7c4d1d94cd850afd44362
Zixuan Wang <33987311+zixuan75@users.noreply.github.com>
2018-12-14 07:24:40 +0800

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-project-homepage
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Welcome here! This is the homepage of The Complex React App.</h1>
+`;
